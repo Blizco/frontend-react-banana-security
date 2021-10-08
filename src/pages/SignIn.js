94001c752cc5b3805d0694cd06422ccfb7cf20d4
Blizco <roel.oldenhuis@kpnmail.nl>
2021-10-08 15:04:32 +0200

    Bonus 1 gedaan

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,9 +1,11 @@
-import React, {useContext} from 'react';
-import {Link} from 'react-router-dom';
-import {AuthContext} from "../context/AuthContext";
+import React, { useContext, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { AuthContext } from "../context/AuthContext";
 
 function SignIn() {
     const {logInFunction} = useContext(AuthContext);
+    const [emailAddress, setEmailAddress] = useState("");
+    const [passWord, setPassWord] = useState("");
 
     return (
         <>
@@ -14,7 +16,26 @@ function SignIn() {
 
 
             <form>
-                <p>*invoervelden*</p>
+                <section>
+                    <label htmlFor = "email-address">Emailadres:</label>
+                    <input
+                    name="email"
+                    id="email-address"
+                    type="email"
+                    value={emailAddress}
+                    onChange={(e) => setEmailAddress(e.target.value)}
+                    />
+                </section>
+                <section>
+                    <label htmlFor = "pass-word">Wachtwoord:</label>
+                    <input
+                        name="password"
+                        id="pass-word"
+                        type="password"
+                        value={passWord}
+                        onChange={(e) => setPassWord(e.target.value)}
+                    />
+                </section>
                 <button type="button"
                         onClick={logInFunction}
                 >
